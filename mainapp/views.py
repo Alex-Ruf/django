c2f79ea7be649863b3e17909ddf5c85f3d334d04
Alex <68159105+Alex-Ruf@users.noreply.github.com>
2021-03-03 22:11:50 +0300

    Revert "new Task2"

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,33 +1,12 @@
 from django.shortcuts import render
 
 def main(request):
-
-    content={
-        'title': 'Главная'
-    }
-    return render(request, 'mainapp/index.html', content)
+    return render(request, 'mainapp/index.html')
 
 def products(request):
-
-    links_menu = [
-        {'href': 'products_all', 'name' :'все' },
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-
-
-    ]
-    content = {
-        'title': 'Продукты',
-        'links_menu': links_menu
-    }
-    return render(request, 'mainapp/products.html', content)
+    return render(request, 'mainapp/products.html')
 
 def contact(request):
-    content = {
-        'title': 'Контакты'
-    }
-    return render(request, 'mainapp/contact.html', content)
+    return render(request, 'mainapp/contact.html')
 
 
